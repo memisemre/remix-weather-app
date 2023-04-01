94f865ae480510dfa05c4197d3f5925d96134caf
Emre Memiş <113227617+memisemre@users.noreply.github.com>
2023-04-01 16:55:32 +0300

    Create HomePage Input Area

--- app/routes/$city.jsx
+++ app/routes/$city.jsx
@@ -10,18 +10,13 @@ export const loader = async({params})=>{
     
     const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${params.city}&appid=${appId}`);
     const data = await response.json();
-    console.log(data)
-    console.log(params)
-    console.log("typeof "+typeof(lat));
-    console.log(`lat${lat}`);
-    console.log(`long ${long}`)
-    console.log("city type "+ typeof(params.city));
+
     if(params.city === "404") throw redirect("/404");
 
     return json({
-    data: "osman"
+    data: data
   });
-  console.log(appId)
+  
 
 }
 export default function Index(){
--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,54 +1,31 @@
+import { useState } from "react";
+
 export const meta = () => {
-  return [{ title: "New Remix App" }];
-};
-
-import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { Outlet } from "@remix-run/react";
-const city = "ankara"
-export const loader = async () => {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=`;
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=`);
-  const data = await response.json();
-  console.log(data)
-
-  return json({
-    data: data
-  });
+  return [{ title: "Remix - Weather App" }];
 };
 
 export default function Index() {
-  
-  console.log("osman")
+  const [searchBarContent,setSearchBarContent] = useState("");
+  const handleInputChange = (e)=>{
+    setSearchBarContent(e.target.value);
+  }
+  const handleSearchBtnClick = ()=>{
+    window.location.href = `/${searchBarContent}`;
+  }
+  const handleKeyPress = (e)=>{
+    e.key === "Enter" && handleSearchBtnClick();
+  }
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-      
+    <div id="welcome-page">
+      <div className="input-container">
+        <input 
+        placeholder="Enter Your City Name" 
+        type="text"
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
+        />
+        <button onClick={handleSearchBtnClick}>Search</button>
+      </div>
     </div>
   );
 }
